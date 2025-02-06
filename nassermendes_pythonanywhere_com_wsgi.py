66a8f2d3d7ffba72dfe1549ecdc8cd0dc4c21601
nassermendes <nassermendes@gmail.com>
2025-02-06 21:22:21 +0000

    Add PythonAnywhere WSGI configuration with environment variables

---
+++ nassermendes_pythonanywhere_com_wsgi.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+# Set environment variables
+os.environ['SECRET_KEY'] = 'algarviocharity-secret-key-2025'
+os.environ['MAIL_USERNAME'] = 'nassermendes@gmail.com'  # Your email
+os.environ['MAIL_PASSWORD'] = 'your-app-specific-password'  # You'll need to generate this in Gmail
+os.environ['MAIL_SERVER'] = 'smtp.gmail.com'
+os.environ['MAIL_PORT'] = '587'
+os.environ['MAIL_USE_TLS'] = 'True'
+
+# Add your project directory to the sys.path
+project_home = '/home/nassermendes/algarviocharity-website'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+# Import your Flask app
+from run import app
+application = app  # This is the WSGI callable
