aefd5b7c81648806ea04f6b11629dc7b15229094
nassermendes <nassermendes@gmail.com>
2025-02-08 23:55:13 +0000

    Add routes for all pages

--- run.py
+++ run.py
@@ -11,6 +11,22 @@ def index():
     else:
         return send_from_directory('.', 'index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/donate')
+def donate():
+    return render_template('donate.html')
+
 @app.route('/<path:path>')
 def serve_static(path):
     # Serve static files, if available, else 404
